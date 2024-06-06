dcd40f5017cc4d45d6d7d1f0169805e807058381
jiwoo <jiwoo7190@naver.com>
2024-06-06 19:03:53 +0900

    change port

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import uvicorn
 
 app = FastAPI()
 
-origins = ["http://127.0.0.1:5501", "http://52.71.209.15:7001"]
+origins = ["http://127.0.0.1:5501", "http://52.71.209.15:8888"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,4 +24,4 @@ async def welcome() -> dict:
 app.include_router(todo_router)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=7000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
